0e0dfebfd375c30d2f2282d36b8cb6aa7defd69f
skyllex94 <45595147+skyllex94@users.noreply.github.com>
2022-03-28 13:58:06 -0400

    minimum votes counted and elimination

--- Runoff/runoff.c
+++ Runoff/runoff.c
@@ -174,20 +174,39 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    // TODO
-    return 0;
+    int min_votes = 100;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (min_votes > candidates[i].votes)
+        {
+            min_votes = candidates[i].votes;
+        }
+    }
+    return min_votes;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (min == candidates[i].votes)
+        {
+            return true;
+        }
+    }
     return false;
 }
 
 // Eliminate the candidate (or candidates) in last place
 void eliminate(int min)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes == min)
+        {
+            candidates[i].eliminated = true;
+        }
+    }
     return;
 }
