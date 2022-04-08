c125c62859b3ce21a8e88cf46aafdc56363872bd
skyllex94 <45595147+skyllex94@users.noreply.github.com>
2022-04-08 13:48:18 -0400

    hash a number to each word given and start to spellcheck it

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -21,33 +21,34 @@ const unsigned int N = 25;
 
 // Hash table
 node *table[N];
-unsigned int hash_value;
+unsigned int hash_index;
 unsigned int word_count;
 
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
-    // TODO
+    unsigned int hashed_text_index = hash(word);
+    // Traverse through each linked list item in the current array[index]
+    for (node *tmp = table[hashed_text_index]; tmp != NULL; tmp = tmp->next)
+    {
+        if (strcasecmp(word, tmp->word) == 0)
+        {
+            return true;
+        }
+    }
     return false;
 }
 
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    if (sizeof(word) != 0)
+    unsigned long hash = 5381;
+    int c;
+    while ((c = toupper(*word++)))
     {
-        for (int i = 97; i < (97 + N); i++)
-        {
-            if (word[0] == i)
-            {
-                node *hashed_word = malloc(sizeof(node));
-                table[i]->word = hashed_word;
-            }
-        }
+        hash = ((hash << 5) + hash) + c;
     }
-
-    // return a_number;
-    return 1;
+    return hash % N;
 }
 
 // Loads dictionary into memory, returning true if successful, else false
@@ -58,7 +59,7 @@ bool load(const char *dictionary)
     if (file != NULL)
     {
         // Initialize a string to populate the data with it can be further used
-        char *dic_word = NULL;
+        char dic_word[LENGTH + 1];
         // Scan each word in the dictionary until the end of the file
         while (fscanf(file, "%c", dic_word) != EOF)
         {
@@ -67,22 +68,27 @@ bool load(const char *dictionary)
             // Copy the content of the current word into memory
             strcpy(current_word->word, dic_word);
             // Hash the current dictionary word
-            char first_letter = 97 + islower(dic_word[0]);
-            for (int i = 0; i < N; i++)
-            {
-                if (strcmp(dic_word[0], first_letter) == 0)
-                {
-                    if (sizeof(table[i]->word) != 0)
-                    {
-                        current_word->next = table[i]->word;
-                        table[i] = current_word;
-                        table[i]->word = current_word->word;
-                    }
-                    table[i] = current_word;
-                    table[i]->word = current_word->word;
-                }
-            }
-            hash(dic_word);
+            hash_index = hash(dic_word);
+            current_word->next = table[hash_index];
+            table[hash_index] = current_word;
+
+            word_count++;
+
+            // char first_letter = 97 + islower(dic_word[0]);
+            // for (int i = 0; i < N; i++)
+            // {
+            //     if (strcmp(dic_word[0], first_letter) == 0)
+            //     {
+            //         if (sizeof(table[i]->word) != 0)
+            //         {
+            //             current_word->next = table[i]->word;
+            //             table[i] = current_word;
+            //             table[i]->word = current_word->word;
+            //         }
+            //         table[i] = current_word;
+            //         table[i]->word = current_word->word;
+            //     }
+            // }
             return true;
         }
     }
