0c35bcd5038324a072b3a1193c76efe85537d42f
skyllex94 <45595147+skyllex94@users.noreply.github.com>
2022-04-13 16:44:56 -0400

    completed credit card recognition for AMEX, Visa and Mastercard

--- credit/credit.c
+++ credit/credit.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 int calculate_long(long number);
+int calculate_int(int cur_num);
 
 int main(void)
 {
@@ -19,11 +20,20 @@ int main(void)
         int exponent_length = calculate_long(exponent);
         if (exponent_length % 2 == 1)
         {
-            checksum_odd += cur_num * 2;
+            checksum_odd += cur_num;
         }
         else
         {
-            checksum_even += cur_num;
+            if (cur_num >= 5)
+            {
+                cur_num = cur_num * 2;
+                int sum_num = calculate_int(cur_num);
+                checksum_even += sum_num;
+            }
+            else
+            {
+                checksum_even += cur_num * 2;
+            }
         }
         exponent *= 10;
         remainder *= 10;
@@ -32,17 +42,23 @@ int main(void)
     int final_check = checksum_odd + checksum_even;
     if (final_check % 10 == 0)
     {
-        if (length == 15)
+        int first_num = (number % (remainder / 10)) / (exponent / 10);
+        int second_num = (number % (remainder / 100)) / (exponent / 100);
+        if (length == 15 && first_num == 3 && (second_num == 4 || second_num == 7))
         {
             printf("AMEX\n");
         }
-        else if (length == 16)
+        else if (length == 16 && first_num == 5 && (second_num == 1 || second_num == 2 || second_num == 3 || second_num == 4 || second_num == 5))
         {
-            printf("MasterCard\n");
+            printf("MASTERCARD\n");
         }
-        else if (length == 13 || length == 16)
+        else if ((length == 13 || length == 16) && first_num == 4)
         {
-            printf("Visa\n");
+            printf("VISA\n");
+        }
+        else
+        {
+            printf("INVALID\n");
         }
     }
     else
@@ -58,9 +74,16 @@ int calculate_long(long number)
 
     while (number >= 10)
     {
-        number = number/10;
+        number = number / 10;
         count++;
     }
     digit = count + 1;
     return digit;
+}
+
+int calculate_int(int cur_num)
+{
+    int sum = (cur_num % 10) / 1;
+    sum = sum + ((cur_num % 100) / 10);
+    return sum;
 }
