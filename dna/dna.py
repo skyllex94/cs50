4a71c7965b6e82089001102349043f21fc458320
skyllex94 <45595147+skyllex94@users.noreply.github.com>
2022-04-19 22:24:40 -0400

    identifying the correct dna_sequence and finds the correct person from which it is

--- dna/dna.py
+++ dna/dna.py
@@ -9,26 +9,54 @@ def main():
         sys.exit("Usage: Input a dataset and a DNA sample to check")
 
     # TODO: Read database file into a variable
+    header_genomes = []
+    dna_compare = []
+    # Read the file and safe the first field names
+    # and loop through each row and save it to a list of dicts
     with open(sys.argv[1], "r") as file:
         reader = csv.DictReader(file)
+        header_genomes = reader.fieldnames
+        # print(header_genomes[2])
         for row in reader:
-            print(f"{row}")
+            dna_compare.append(row)
+        # print(dna_compare)
 
     # TODO: Read DNA sequence file into a variable
-    # dna_strand = []
     with open(sys.argv[2]) as txtfile:
         dna_strand = txtfile.readlines()
-    print(dna_strand)
 
     # TODO: Find longest match of each STR in DNA sequence
-    split_genome = []
     dna_string = dna_strand[0]
-    for genome in range(0, len(dna_string), 4):
-        split_genome.append(dna_string[genome: genome + 4])
-    print(f"{split_genome}_")
+    # print(f"{dna_string}")
+
+    longest_run = []
+    # Create a loop to call the longest match function for the sequence
+    # and each header field without the name field
+    for i in range(1, len(header_genomes)):
+        longest_run.append(longest_match(dna_string, header_genomes[i]))
+    # print(f"{longest_run}")
 
     # TODO: Check database for matching profiles
+    dna_match = ""
 
+    # Take each dict item from the list of dicts
+    for i in range(0, len(dna_compare)):
+        dict_values_list = list(dna_compare[i].values())
+        # Split the list in half to compare only the numbers
+        name_part = dict_values_list[:1]
+        dna_part = dict_values_list[1:]
+
+        # Convert each element into a number
+        for j in range(len(dna_part)):
+            dna_part[j] = int(dna_part[j])
+        # Compare the longest run list with the dna_part of the list
+        if dna_part == longest_run:
+            dna_match = name_part[0]
+
+    if dna_match == "":
+        print("No match")
+    else:
+        print(dna_match)
     return
 
 
@@ -45,7 +73,6 @@ def longest_match(sequence, subsequence):
 
         # Initialize count of consecutive runs
         count = 0
-
         # Check for a subsequence match in a "substring" (a subset of characters) within sequence
         # If a match, move substring to next potential match in sequence
         # Continue moving substring and checking for matches until out of consecutive matches
